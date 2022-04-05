27491c95b11e6206ddede09d634eeec99114c7a6
Ignolek <ignole@icloud.com>
2022-04-05 20:29:20 +0200

    First step into openCV (transform to grayscale)

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import sys
+import cv2 as cv
 
 
 # webhook test
 
 def main() -> None:
     print("Hello world")
+    img = cv.imread('Finger/testFinger.jpg')
+
+    cv.imshow('Default', img)
+
+    # Converting to grayscale
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow('Gray', gray)
+
+    cv.waitKey(0)
 
     sys.exit(0)
 
