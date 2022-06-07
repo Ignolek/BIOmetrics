9831c3362e35fa040369ce26646b161941558599
WDobies <68401136+WDobies@users.noreply.github.com>
2022-06-07 22:52:54 +0200

    Lil cleanup

--- voiceR.py
+++ voiceR.py
@@ -1,25 +1,24 @@
-from os import remove
+
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
-import numpy as np
 import statistics
 
-file = "voices/FHRO_Sr31.wav"
+file = "voices/MTLS_Sr18.wav"
 
-signal, sr = librosa.load(file, mono=True)
+signal, sr = librosa.load(file)
 
-f0 = librosa.yin(signal, fmin = 80, fmax= 400)
+f0 = librosa.yin(signal, fmin = 60, fmax= 400)
 
 times = librosa.times_like(f0)
 
-print(statistics.median(f0))
+print(statistics.median(f0), "Hz")
 if statistics.median(f0)> 165: print("female")
 else: print("male")
 
 fig, ax = plt.subplots()
 ax.set(title='YIN fundamental frequency estimation')
-ax.plot(times, f0, label='x', color='blue', linewidth=1)
+ax.plot(times, f0, label='f0', color='blue', linewidth=1)
 ax.legend(loc='upper right')
 plt.show()
 
